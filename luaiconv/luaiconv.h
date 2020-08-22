bc960ec1db8552d98b78e5dff76d7291f08e489e
initializeZero <1085473804@qq.com>
2020-08-22 15:56:05 +0800

    add luaiconv

---
+++ luaiconv/luaiconv.h
@@ -0,0 +1,13 @@
+﻿#ifndef _LUAICONV_H
+#define _LUAICONV_H
+
+#include <lua.h>
+#if defined(_WIN32) && defined(_MSC_VER)
+
+#define DLL_VARIABLE __declspec(dllexport)
+#pragma comment(lib,"libiconv.lib")
+#else
+#define DLL_VARIABLE __attribute__((__visibility__("default")))
+#endif
+DLL_VARIABLE int luaopen_iconv(lua_State *L);
+#endif //ANDLUA_LUAICONV_H
